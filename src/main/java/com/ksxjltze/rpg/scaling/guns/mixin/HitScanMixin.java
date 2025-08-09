568bf75ca6b820115cf39c1e52e66d7a5d8d965d
Lee Jia Keat <leejiakeat@gmail.com>
2025-08-09 16:48:38 +0800

    aaadada

--- src/main/java/com/ksxjltze/rpg/scaling/guns/mixin/HitScanMixin.java
+++ src/main/java/com/ksxjltze/rpg/scaling/guns/mixin/HitScanMixin.java
@@ -1,20 +1,28 @@
 package com.ksxjltze.rpg.scaling.guns.mixin;
 
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(com.vicmatskiv.pointblank.client.ClientSystem.class)
-public class HitScanMixin {
+@Mixin(value = com.vicmatskiv.pointblank.item.HurtingItem.class)
+public abstract class HitScanMixin extends Item {
 
-    @Inject(
-            at = @At("TAIL"),
-            method = "<init>()V"
-    )
-    private void init(CallbackInfo info) {
-        System.out.println("Ten Yu is so gae!");
+    public HitScanMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Shadow
+    protected float damage;
+
+    @Inject(method = "getDamage", at = @At("RETURN"), cancellable = true)
+    public void onGetDamage(CallbackInfoReturnable<Float> cir) {
+        System.out.println("Ten Yu is so gae!!");
+
+        if (cir.getReturnValue() == null)
+            cir.setReturnValue(this.damage);
     }
 }
